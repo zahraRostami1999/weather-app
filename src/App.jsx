1e691bfb1f84902e58f30c96c2c6e2524b06df78
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-26 16:59:56 +0330

    edit App.jsx: add select tag for choose city

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
 import "./App.css";
 
-function App() {
-  const cities = [
-    {
-      name: "Tehran",
-      lat: "35.7219",
-      lon: "51.3347",
-    },
-    {
-      name: "Ardbil",
-      lat: "38.2432",
-      lon: "48.2976",
-    },
-    {
-      name: "Tokyo",
-      lat: "35.6764",
-      lon: "139.6500",
-    }
-  ];
+const cities = [
+  {
+    name: "Tehran",
+    lat: "35.7219",
+    lon: "51.3347",
+  },
+  {
+    name: "Ardbil",
+    lat: "38.2432",
+    lon: "48.2976",
+  },
+  {
+    name: "Tokyo",
+    lat: "35.6764",
+    lon: "139.6500",
+  },
+];
 
-  return <div className="App"></div>;
+function App() {
+
+  return (
+    <div className="App">
+      <select onchange={handleChange}>
+        {cities.map((city) => (
+          <option key={city.name} value={JSON.stringify(city)}>
+            {city.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
 
 export default App;
