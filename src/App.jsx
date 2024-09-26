f617bee37f614ab2d3f58d024181f2a8abb71f38
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-26 16:29:56 +0330

    edit App.jsx: define cities list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
+  const cities = [
+    {
+      name: "Tehran",
+      lat: "35.7219",
+      lon: "51.3347",
+    },
+    {
+      name: "Ardbil",
+      lat: "38.2432",
+      lon: "48.2976",
+    },
+    {
+      name: "Tokyo",
+      lat: "35.6764",
+      lon: "139.6500",
+    }
+  ];
 
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
