99dbed227b9c8c6d5d56441fa86e7daf7db9d69e
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-28 21:21:04 +0330

    delete API.ts file

--- src/API.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const key = "05086f9c1b192c3ea544760f9f7c73d2";
-
-export async function getCurrentWeather(lat, lon){
-    await axios(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)
-}
