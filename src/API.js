e4a13858f228c17cc9836c5012216c2ca5870156
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-29 12:13:17 +0330

    remove API file

--- src/API.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const APIkey = "05086f9c1b192c3ea544760f9f7c73d2";
-
-export async function getCurrentWeather(lat, lon){
-    const {data} = await axios(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`);
-
-    return data;
-}
