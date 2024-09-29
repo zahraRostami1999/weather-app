479c5c67a2211cbd3c767f657f2d5d969088d1b1
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-29 12:14:15 +0330

    edit App.jsx file: remove previous code

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,11 @@
-import { getCurrentWeather } from "./API.js";
-import "./App.css";
-
-const cities = [
-  {
-    name: "Tehran",
-    lat: "35.7219",
-    lon: "51.3347",
-  },
-  {
-    name: "Ardbil",
-    lat: "38.2432",
-    lon: "48.2976",
-  },
-  {
-    name: "Tokyo",
-    lat: "35.6764",
-    lon: "139.6500",
-  },
-];
+import React from 'react'
 
 function App() {
-
-  const handleChange = (e) => {
-    const cityValue = e.target.value; // cityValue Type = string
-    const setCity = JSON.parse(cityValue); // setCity Type = object
-    
-    getCurrentWeather(setCity.lat, setCity.lon).then((weather) => {
-      console.log(weather); 
-    });
-
-    
-  };
   return (
-    <div className="App">
-      <h1>Welcome To Weather App</h1>
-      <h3>Select your location</h3>
-      <select onChange={handleChange}>
-        {cities.map((city) => (
-          <option key={city.name} value={JSON.stringify(city)}>
-            {city.name}
-          </option>
-        ))}
-      </select>
+    <div className='app'>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
