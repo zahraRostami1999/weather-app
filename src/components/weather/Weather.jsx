433ea7ae136858ca67aa9ffc4070b110f5218a95
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-30 20:28:25 +0330

    edit weather component: define

---
+++ src/components/weather/Weather.jsx
@@ -0,0 +1,11 @@
+import styled from './weather.module.css';
+
+function Weather(){
+    return(
+        <div className={styled.container}>
+            This is Weather App
+        </div>
+    );
+}
+
+export default Weather;
