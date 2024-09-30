cf5c2c9f2f199fd71f61dcc0e5408afa7d4b2c18
Zahra Rostami <zahrarostami137878@gmail.com>
2024-09-30 22:39:27 +0330

    edit weather file: add weather function

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -4,6 +4,21 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faSun } from "@fortawesome/free-solid-svg-icons";
 
 function Weather() {
+
+    const weather = () => {
+        const search = async (city) => {
+        try{
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.API_ID}`
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data); // Display the weather data in the console for testing purposes
+        }
+        catch(error){
+            console.error("Error fetching weather data:", error);
+        }
+    }
+}
+
   return (
     <div className={styled.container}>
       <div className={styled.weatherWrapper}>
