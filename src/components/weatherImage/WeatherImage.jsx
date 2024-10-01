f3f80a0da71f88f7d6d000e49b985291096494d5
Zahra Rostami <zahrarostami137878@gmail.com>
2024-10-01 10:47:37 +0330

    edit weatherImage component: import images

--- src/components/weatherImage/WeatherImage.jsx
+++ src/components/weatherImage/WeatherImage.jsx
@@ -1,4 +1,13 @@
 import styled from './weatherImage.module.css';
+import clear from '../../assets/image/01.png';
+import fewClouds from '../../assets/image/02.png';
+import scatteredClouds from '../../assets/image/03.png';
+import brokenClouds from '../../assets/image/04.png';
+import 	showerRain from '../../assets/image/09.png';
+import rain from '../../assets/image/10.png';
+import thunderstorm from '../../assets/image/11.png';
+import snow from '../../assets/image/13.png';
+import 	mist from '../../assets/image/50.png';
 
 function WeatherImage(){
     return(
