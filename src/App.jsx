9fae226dfc03c64cbd2b7c7a1aae4944958221ec
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-24 17:19:14 +0330

    edit App: change doc title

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Weather from './components/weather/Weather.jsx';
 
 function App() {
+  document.title = "Weather"
   return (
     <div className='app'>
       <Weather />
